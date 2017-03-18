46a95fff6ae3be0f8484a77a36d68b0378165583
Hugh Edwards <hugz98@hotmail.com>
2017-03-18 14:20:58 +1100

    Minor changes

--- lifeline/urls.py
+++ lifeline/urls.py
@@ -3,12 +3,15 @@ from django.contrib.auth.views import login
 
 from . import views
 
+app_name="lifeline"
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
         # ex: /lifeline/map/
-	url(r'^map/$', views.map),
-	url(r'^news_feed/$', views.news_feed),
-	url(r'^item/(?P<item_id>[0-9]+)/$', views.item, name='vote'),
-	url(r'^create/$', views.create),
+	url(r'^map/$', views.map,name="map"),
+	url(r'^news_feed/$', views.news_feed,name="news_feed"),
+	url(r'^item/(?P<item_id>[0-9]+)/$', views.item, name='item'),
+	url(r'^create/$', views.create,name="create"),
+	url(r'^submitted/$', views.submitted, name="submitted"),
     url('^', include('django.contrib.auth.urls'))
 ]
