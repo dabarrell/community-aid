fec6fd315c80d7fafceb77cbcdcc8b7eb22fb411
Hugh Edwards <hugz98@hotmail.com>
2017-03-18 19:27:08 +1100

    Comments work, BRUH

--- lifeline/urls.py
+++ lifeline/urls.py
@@ -8,8 +8,9 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
         # ex: /lifeline/map/
 	url(r'^map/$', views.map,name="map"),
-	url(r'^item/(?P<item_id>[0-9]+)/$', views.item, name='item'),
+	url(r'^item/(?P<item_id>[0-9a-f\-]+)/$', views.item, name='item'),
 	url(r'^create/$', views.create,name="create"),
 	url(r'^submitted/$', views.submitted, name="submitted"),
+	url(r'^comment_submitted/$', views.comment_submitted, name="comment_submitted"),
     url('^', include('django.contrib.auth.urls'))
 ]
--- lifeline/views.py
+++ lifeline/views.py
@@ -19,19 +19,24 @@ def index(request):
 
 def item(request, item_id):
 
+	#display location on map
+
 	#get rid of the dummy ID here
 
-	DUMMY_ID='69abafa5-1179-4a2f-b5ab-e02508d94445'
+	DUMMY_ID='7f8de92e-efc2-4c69-8f48-8612fab4b3a6'
 
 	item = DUMMY_ID
 
 	context = {
+		'item_id': item,
 		'item_name': Item.objects.get(pk=item).item_name,
 		'item_description': Item.objects.get(pk=item).item_description,
 		'item_priority': Item.objects.get(pk=item).item_priority,
 		'item_category': Item.objects.get(pk=item).item_category,
 		'item_type': Item.objects.get(pk=item).item_type,
-		'item_location': Item.objects.get(pk=item).item_location,
+		'item_longitude': Item.objects.get(pk=item).item_longitude,
+		'item_latitude': Item.objects.get(pk=item).item_latitude,
+		'comments' : Comment.objects.filter(item_id=item)
 
 	}
 	template = loader.get_template('lifeline/item.html')
@@ -43,6 +48,19 @@ def submitted(request):
 	context = {"item_name":request.POST.get("item_name")}
 	submit_item(request.POST, user)
 	template = loader.get_template('lifeline/submitted.html')
+
+	return HttpResponse(template.render(context, request))
+
+
+def comment_submitted(request):
+
+	user = User.objects.get(user=request.user)
+	context = {
+		"comment_text":request.POST.get("comment_text"),
+		"item":request.POST.get("item")
+		}
+	submit_comment(request.POST, user)
+	template = loader.get_template('lifeline/comment_submitted.html')
 
 	return HttpResponse(template.render(context, request))
 
@@ -90,5 +108,17 @@ def submit_item(post, user):
 		item_category=Item_Category.objects.get(category_name=category_string),
 		item_type=Item_Type.objects.get(type_name=type_string),
 
-	)#=post.get('item_name'))
+	)
 	item.save()
+
+
+#add datetime
+
+def submit_comment(post,user):
+	comment = Comment(
+		item_id = post.get('item'),
+		comment_text = post.get('comment_text'),
+		user = user
+	)
+	comment.save()
+
