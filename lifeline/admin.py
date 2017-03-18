baddd57a418d00229f1b3f4ed43b3c18fb659459
David Barrell <david@barrell.me>
2017-03-18 12:16:07 +1100

    Fixed models, added admin etc

--- lifeline/admin.py
+++ lifeline/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+
+from .models import Item, Comment, Item_Type, Item_Category, Item_Priority
+
+admin.site.register(Item)
+admin.site.register(Comment)
+admin.site.register(Item_Type)
+admin.site.register(Item_Category)
+admin.site.register(Item_Priority)
+
 # Register your models here.
--- lifeline/models.py
+++ lifeline/models.py
@@ -1,17 +1,5 @@
 from django.db import models
 
-class Item(models.Model):
-	item_name = models.CharField(max_length=100)
-	created_at = models.DateTimeField(auto_now_add=True)
-	updated_at = models.DateTimeField(auto_now=True)
-	author_name = models.CharField(max_length=100)
-	author_phone = models.Charfield(max_length=12)
-	item_description = models.Charfield(max_length=500)
-	item_location = models.CharField(max_length=100) #change this to the location type
-	item_priority = models.ForeignKey(Item_Priority, on_delete=models.CASCADE)
-	item_category = models.ForeignKey(Item_Category, on_delete=models.CASCADE)
-	item_type = models.ForeignKey(Item_Type, on_delete=models.CASCADE)
-
 
 class Item_Priority(models.Model):
 	priority_name = models.CharField(max_length=20)
@@ -21,12 +9,27 @@ class Item_Category(models.Model):
 
 class Item_Type(models.Model):
 	type_name = models.CharField(max_length=20)
+	
+class Item(models.Model):
+	item_name = models.CharField(max_length=100)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+	author_name = models.CharField(max_length=100)
+	author_phone = models.CharField(max_length=12)
+	item_description = models.CharField(max_length=500)
+	item_location = models.CharField(max_length=100) #change this to the location type
+	item_priority = models.ForeignKey(Item_Priority, on_delete=models.CASCADE)
+	item_category = models.ForeignKey(Item_Category, on_delete=models.CASCADE)
+	item_type = models.ForeignKey(Item_Type, on_delete=models.CASCADE)
+
+
+
 
 
 class Comment(models.Model):
 	comment_text = models.CharField(max_length=500)
 	author_name = models.CharField(max_length=100)
-	author_phone = models.Charfield(max_length=12)
+	author_phone = models.CharField(max_length=12)
 	created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+	updated_at = models.DateTimeField(auto_now=True)
 	item = models.ForeignKey(Item, on_delete=models.CASCADE)
