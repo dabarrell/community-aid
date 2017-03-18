48538dc91fb46dbf505cf55bb8b306e168294849
Hugh Edwards <hugz98@hotmail.com>
2017-03-18 11:39:09 +1100

    Added basic models - let me know if its all g

--- lifeline/models.py
+++ lifeline/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+	item_name = models.CharField(max_length=100)
+	pub_date = models.DateTimeField('date published')
+	author_name = models.CharField(max_length=100)
+	author_phone = models.Charfield(max_length=12)
+	item_description = models.Charfield(max_length=500)
+	item_location = models.CharField(max_length=100) #change this to the location type
+	item_priority = models.ForeignKey(Item_Priority, on_delete=models.CASCADE)
+	item_category = models.ForeignKey(Item_Category, on_delete=models.CASCADE)
+	item_type = models.ForeignKey(Item_Type, on_delete=models.CASCADE)
+
+
+class Item_Priority(models.Model):
+	priority = models.CharField(max_length=20)
+
+class Item_Category(models.Model):
+	priority = models.CharField(max_length=40)
+
+class Item_Type(models.Model):
+	priority = models.CharField(max_length=20)
+
+
+class Comment(models.Model):
+	comment_text = models.CharField(max_length=500)
+	author_name = models.CharField(max_length=100)
+	author_phone = models.Charfield(max_length=12)
+	pub_date = models.DateTimeField('date published')
+	item = models.ForeignKey(Item, on_delete=models.CASCADE)
