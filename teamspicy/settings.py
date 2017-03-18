c33ae69e02e32ad07c2fff84033dcbc8014cc2b4
David Barrell <david@barrell.me>
2017-03-18 11:13:11 +1100

    Update shit

--- teamspicy/settings.py
+++ teamspicy/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'lifeline.apps.LifelineConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
